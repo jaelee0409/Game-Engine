faa5ed2cea60f8c1db757b2bebe5d6dec9696cd1
Jae Lee <jaeslee@utexas.edu>
2022-07-14 21:15:13 -0500

    Added Project Generation with Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Terran"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Terran"
+	location "Terran"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"TR_PLATFORM_WINDOWS",
+			"TR_BUILD_DLL"
+		}
+	
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "TR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "TR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Terran/vendor/spdlog/include",
+		"Terran/src"
+	}
+
+	links
+	{
+		"Terran"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"TR_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "TR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "TR_DIST"
+		optimize "On"
