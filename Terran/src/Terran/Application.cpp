eef6cdac543ad75015f4aef7a30af38c08a1bbea
Jae Lee <jaeslee@utexas.edu>
2022-07-21 03:27:45 -0500

    Added Vertex Buffer Layouts

--- Terran/src/Terran/Application.cpp
+++ Terran/src/Terran/Application.cpp
@@ -10,127 +10,183 @@ namespace Terran {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-	Application* Application::s_Instance = nullptr;
+    Application* Application::s_Instance = nullptr;
 
-	Application::Application()
-	{
-		TR_CORE_ASSERT(!s_Instance, "Application already exists");
-		s_Instance = this;
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case ShaderDataType::Float:
+                return GL_FLOAT;
+            case ShaderDataType::Float2:
+                return GL_FLOAT;
+            case ShaderDataType::Float3:
+                return GL_FLOAT;
+            case ShaderDataType::Float4:
+                return GL_FLOAT;
+            case ShaderDataType::Mat3:
+                return GL_FLOAT;
+            case ShaderDataType::Mat4:
+                return GL_FLOAT;
+            case ShaderDataType::Int:
+                return GL_INT;
+            case ShaderDataType::Int2:
+                return GL_INT;
+            case ShaderDataType::Int3:
+                return GL_INT;
+            case ShaderDataType::Int4:
+                return GL_INT;
+            case ShaderDataType::Bool:
+                return GL_BOOL;
+        }
 
-		m_Window = std::unique_ptr<Window>(Window::Create());
-		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+        TR_CORE_ASSERT(false, "Unknown ShaderDataType");
+        return 0;
+    }
 
-		m_ImGuiLayer = new ImGuiLayer();
-		PushOverlay(m_ImGuiLayer);
+    Application::Application()
+    {
+        TR_CORE_ASSERT(!s_Instance, "Application already exists");
+        s_Instance = this;
 
-		glGenVertexArrays(1, &m_VertexArray);
-		glBindVertexArray(m_VertexArray);
+        m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
-		};
+        m_ImGuiLayer = new ImGuiLayer();
+        PushOverlay(m_ImGuiLayer);
 
-		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+        glGenVertexArrays(1, &m_VertexArray);
+        glBindVertexArray(m_VertexArray);
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 0.8f, 0.0f, 0.8f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+            0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
+        };
 
-		uint32_t indices[3] = {
-			0, 1, 2
-		};
+        m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(vertices) / sizeof(uint32_t)));
+        {
+            BufferLayout layout = {
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" }
+            };
+            m_VertexBuffer->SetLayout(layout);
+        }
 
-		std::string vertexSrc = R"(
-			#version 330 core
+        uint32_t index = 0;
+        const auto& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout)
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index,
+                                  element.GetComponentCount(),
+                                  ShaderDataTypeToOpenGLBaseType(element.Type),
+                                  element.Normalized ? GL_TRUE : GL_FALSE,
+                                  layout.GetStride(),
+                                  (const void*)element.Offset);
+            index++;
+        }
 
-			layout(location = 0) in vec3 a_Position;
+        uint32_t indices[3] = {
+            0, 1, 2
+        };
 
-			out vec3 v_Position;
+        m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(vertices) / sizeof(uint32_t)));
 
-			void main()
-			{
-				v_Position = a_Position + 0.5;
-				gl_Position = vec4(a_Position, 1.0);
-			}
-		)";
+        std::string vertexSrc = R"(
+            #version 330 core
 
-		std::string fragmentSrc = R"(
-			#version 330 core
+            layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
-			layout(location = 0) out vec4 color;
+            out vec3 v_Position;
+            out vec4 v_Color;
 
-			in vec3 v_Position;
+            void main()
+            {
+                v_Position = a_Position + 0.5;
+                v_Color = a_Color;
+                gl_Position = vec4(a_Position, 1.0);
+            }
+        )";
 
-			void main()
-			{
-				color = vec4(v_Position, 1.0);
-			}
-		)";
+        std::string fragmentSrc = R"(
+            #version 330 core
 
-		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
-	}
+            layout(location = 0) out vec4 color;
 
-	Application::~Application()
-	{
+            in vec3 v_Position;
+            in vec4 v_Color;
 
-	}
+            void main()
+            {
+                color = vec4(v_Position, 1.0);
+                color = v_Color;
+            }
+        )";
 
-	void Application::OnEvent(Event& e)
-	{
-		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+        m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
+    }
 
-		//TR_CORE_TRACE("{0}", e);
+    Application::~Application()
+    {
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
-		{
-			(*--it)->OnEvent(e);
-			if (e.Handled)
-				break;
-		}
-	}
+    }
 
-	void Application::Run()
-	{
-		while (m_Running)
-		{
-			glClearColor(0.1f, 0.1f, 0.1f, 1);
-			glClear(GL_COLOR_BUFFER_BIT);
+    void Application::OnEvent(Event& e)
+    {
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-			m_Shader->Bind();
-			//glBindVertexArray(m_VertexArray);
-			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
+        //TR_CORE_TRACE("{0}", e);
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate();
+        for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+        {
+            (*--it)->OnEvent(e);
+            if (e.Handled)
+                break;
+        }
+    }
 
-			m_ImGuiLayer->Begin();
-			for (Layer* layer : m_LayerStack)
-				layer->OnImGuiRender();
-			m_ImGuiLayer->End();
+    void Application::Run()
+    {
+        while (m_Running)
+        {
+            glClearColor(0.1f, 0.1f, 0.1f, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
 
-			m_Window->OnUpdate();
-		}
-	}
+            m_Shader->Bind();
+            //glBindVertexArray(m_VertexArray);
+            glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
 
-	void Application::PushLayer(Layer* layer)
-	{
-		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
-	}
+            for (Layer* layer : m_LayerStack)
+                layer->OnUpdate();
 
-	void Application::PushOverlay(Layer* layer)
-	{
-		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
-	}
+            m_ImGuiLayer->Begin();
+            for (Layer* layer : m_LayerStack)
+                layer->OnImGuiRender();
+            m_ImGuiLayer->End();
 
-	bool Application::OnWindowClose(WindowCloseEvent& e)
-	{
-		m_Running = false;
-		return true;
-	}
+            m_Window->OnUpdate();
+        }
+    }
+
+    void Application::PushLayer(Layer* layer)
+    {
+        m_LayerStack.PushLayer(layer);
+        layer->OnAttach();
+    }
+
+    void Application::PushOverlay(Layer* layer)
+    {
+        m_LayerStack.PushOverlay(layer);
+        layer->OnAttach();
+    }
+
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
+    }
 }
